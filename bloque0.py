77860a19b6a14c91e4919660831825d3da3dc4dc
martinmedina12 <mnmedina@estud.usfq.edu.ec>
2026-04-13 17:14:09 -0500

    Importar libreria pandas y matplotlib

---
+++ bloque0.py
@@ -0,0 +1,7 @@
+pip install pandas
+pip install matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+print('pandas version:', pd.__version__)
+print('matplotlib importado correctamente')
+print('Todo listo para el taller')
